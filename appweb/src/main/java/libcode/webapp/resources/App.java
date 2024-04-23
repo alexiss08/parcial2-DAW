9ae5d7c42d8da7792e1a6707a06acf958e28a158
alexis08 <alexis08miranda@gmail.com>
2024-04-23 10:58:16 -0600

    crud con java enterprise

---
+++ appweb/src/main/java/libcode/webapp/resources/App.java
@@ -0,0 +1,10 @@
+package libcode.webapp.resources;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+
+@ApplicationPath("/api")
+public class App extends Application {
+    
+}
